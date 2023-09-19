9815541e1cb486d063734e13efc61722220a0d60
Luna <hernandezguerraluna@gmail.com>
2023-09-19 17:14:54 +0100

    Arrays and String Methods, ArrayList and 2D Arrays

--- 4. Arrays and String Methods/Arrays/4.3 Get Letter List/src/LettersList.java
+++ 4. Arrays and String Methods/Arrays/4.3 Get Letter List/src/LettersList.java
@@ -1,5 +1,13 @@
 public class LettersList
 {
     //code the getLetterList method that accepts a word as argument and returns a list of its letters
-    /* Complete the getLettersList method that get the list of letters in the input word. */
+    public static String[] getLettersList(String word)
+    {
+        String[] result = new String[word.length()];
+        for (int i = 0; i < word.length(); i++)
+        {
+            result[i] = word.substring(i, i+1);
+        }
+        return result;
+    }
 }
--- 5. ArrayList and 2D Arrays/2D Arrays/5.3 Chessboard/src/Chessboard.java
+++ 5. ArrayList and 2D Arrays/2D Arrays/5.3 Chessboard/src/Chessboard.java
@@ -4,6 +4,21 @@ public class Chessboard
    {
       char[][] chessboard = new char[8][8];
 
-      /* Create and print a chessboard. */
+      for (int row = 0; row < chessboard.length; row++) {
+         for (int col = 0; col < chessboard.length; col++) {
+            if ((row+col) % 2 == 0) {
+               chessboard[row][col] = '\u25A1';
+            }
+            else {
+               chessboard[row][col] = '\u25A0';
+            }
+         }
+      }
+      for (int row = 0; row < chessboard.length; row++) {
+         for (int col = 0; col < chessboard.length; col++) {
+               System.out.print(chessboard[row][col]);
+         }
+         System.out.println("");
+      }
    }
 }
